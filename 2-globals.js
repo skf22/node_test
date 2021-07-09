668cd6798809b8ec7bdc082a5d623f80d92eb8f1
Shirjeel Khalil Fahid <skf11987@gmail.com>
2021-07-09 20:40:21 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,6 @@
+console.log(__dirname);
+console.log(__filename);
+
+setInterval(() => {
+  console.log("Hullo Dob");
+}, 1000);
---
+++ 5-utils.js
@@ -0,0 +1,6 @@
+const sayHi = (name) => {
+  name.toUpperCase();
+  console.log(`Hey there, ${name}`);
+};
+
+module.exports = sayHi;
